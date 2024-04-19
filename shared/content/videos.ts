3c37cf465ceeed240d1cdafb8f35d0441cc9fa4e
Kalidou Diagne <kld.diagne@gmail.com>
2024-04-19 16:56:42 +0200

    fix videos

--- shared/content/videos.ts
+++ shared/content/videos.ts
@@ -5,12 +5,12 @@ export const VIDEOS: BlogItem[] = [
     url: "https://www.youtube.com/watch?v=SBlxm2Ny_yI&list=PLXzKMXK2aHh4a3KLC4_2MeI4CG6qj1Vmq&index=12&ab_channel=ETHGlobal",
     title: "Ethereum Foundation 🛠 Introduction to Anon Aadhaar SDK", // ETHIndia
     date: new Date("2024-01-15"),
-    eventName: "ETH India",
+    label: "ETH India",
   },
   {
     url: "https://www.youtube.com/watch?v=OEXWm4GID6U&list=PLXzKMXK2aHh7yE-ZUnzm0y-d6HweyGYkJ&index=6&ab_channel=ETHGlobal",
     title: "PSE - Ethereum Foundation 🛠️ Anon Aadhaar Workshop", // Circuit Breaker
     date: new Date("2024-02-15"),
-    eventName: "Circuit Breaker",
+    label: "Circuit Breaker",
   },
 ];
