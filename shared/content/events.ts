e48be9e6f8bf433a7b18d2fa0bf2bc8471b313bc
Kalidou Diagne <kld.diagne@gmail.com>
2024-05-07 13:52:28 +0900

    fix copy for events

--- shared/content/events.ts
+++ shared/content/events.ts
@@ -8,22 +8,19 @@ export interface EventItem {
 export const EVENTS: EventItem[] = [
   {
     eventName: "EthCC",
-    description:
-      "Bandada team will attend Progcrypto and deliver workshops on how to build applications with Bandada.",
+    description: "Anon Aadhaar team will attend EthCC.",
     date: "July 8 - 11, 2024",
     url: "https://ethcc.io/",
   },
   {
     eventName: "Devcon",
-    description:
-      "Bandada team will attend Progcrypto and deliver workshops on how to build applications with Bandada.",
+    description: "Anon Aadhaar team will attend Devcon.",
     date: "Nov 09 - 17, 2024",
     url: "https://devcon.org/en/",
   },
   {
     eventName: "ETHIndia",
-    description:
-      "Bandada team will attend Progcrypto and deliver workshops on how to build applications with Bandada.",
+    description: "Anon Aadhaar team will attend ETHIndia.",
     date: "Nov 09 - 17, 2024",
     url: "#",
   },
