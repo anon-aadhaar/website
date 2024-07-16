9c449a70805c33ca18d9be217558609d743c4aae
Kalidou Diagne <kld.diagne@gmail.com>
2024-07-16 21:21:40 +0200

    add matomo tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { AppHeader } from "@/components/header/AppHeader";
 import { rajdhani, inter } from "@/shared/fonts";
 import { AppFooter } from "@/components/AppFooter";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Anon Aadhaar",
@@ -33,6 +34,23 @@ export default function RootLayout({
       <body
         className={`${rajdhani.className} ${rajdhani.variable} ${inter.variable}`}
       >
+        <head>
+          <Script id="matomo-tracking" strategy="afterInteractive">
+            {`
+            var _paq = window._paq = window._paq  [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="https://psedev.matomo.cloud/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '15']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src='https://cdn.matomo.cloud/psedev.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `}
+          </Script>
+        </head>
         <AppHeader />
         <div className="overflow-x-hidden">{children}</div>
         <AppFooter />
